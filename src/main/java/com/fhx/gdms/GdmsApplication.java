754dedcca211b0a61c73e49dd3de619cd86f4294
JustForAPromise <13414105154@163.com>
2018-12-03 23:04:06 +0800

    init projection

---
+++ src/main/java/com/fhx/gdms/GdmsApplication.java
@@ -0,0 +1,12 @@
+package com.fhx.gdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdmsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/fhx/gdms/GdmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fhx.gdms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GdmsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
