c4f0d490bdcff66ead6eefda67598b32790ee219
fanghx@figo.cn <fanghx@figo.cn>
2019-03-06 16:17:47 +0800

    2019-3-6

---
+++ src/main/java/com/fhx/gdms/supportUtil/FileUtil.java
@@ -0,0 +1,37 @@
+package com.fhx.gdms.supportUtil;
+
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * @Auther fanhanxi
+ * @Date 2019/3/6
+ * @Description:
+ */
+public class FileUtil {
+
+    public static String getTaskBookFileDir() {
+        File path = null;
+        try {
+            path = new File(ResourceUtils.getURL("classpath:").getPath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String filePath = path.getParentFile().getParentFile().getParent()
+                + File.separator + "upload" + File.separator + "taskBook" + File.separator;
+        return filePath;
+    }
+    public static String getThesesFileDir() {
+        File path = null;
+        try {
+            path = new File(ResourceUtils.getURL("classpath:").getPath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String filePath = path.getParentFile().getParentFile().getParent()
+                + File.separator + "upload" + File.separator + "theses" + File.separator;
+        return filePath;
+    }
+}
--- src/main/java/com/fhx/gdms/taskbook/controllers/TaskBookController.java
+++ src/main/java/com/fhx/gdms/taskbook/controllers/TaskBookController.java
@@ -1,6 +1,7 @@
 package com.fhx.gdms.taskbook.controllers;
 
 import com.fhx.gdms.supportUtil.ApiResult;
+import com.fhx.gdms.supportUtil.FileUtil;
 import com.fhx.gdms.taskbook.service.TaskBookService;
 import com.fhx.gdms.user.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +10,21 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 @RequestMapping("/taskBook")
 public class TaskBookController {
+
     @Autowired
     private TaskBookService taskBookService;
 
@@ -25,19 +33,26 @@ public class TaskBookController {
 
     @RequestMapping(value = "/add", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    ApiResult save(File file) {
+    ApiResult save(@RequestParam("file") MultipartFile file) {
         ApiResult apiResult = new ApiResult();
 
-        UserModel userInfo = (UserModel) session.getAttribute("userInfo");
-
-        if (model != null) {
-            apiResult.setCode(0);
-            apiResult.setMsg("提交成功！待导师检阅");
-            apiResult.setData(model);
-        } else {
+        if (file.isEmpty()) {
             apiResult.setCode(-1);
-            apiResult.setMsg("ERROR");
+            apiResult.setMsg("Please select a file to upload");
+            return apiResult;
         }
-        return apiResult;
+
+        String uploadDir = FileUtil.getThesesFileDir();
+
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(uploadDir + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
